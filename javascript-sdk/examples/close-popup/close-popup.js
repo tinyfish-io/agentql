c118d08773658668526d4f876c6e2ad77188ccfc
SuveenE-TF <suveen@tinyfish.io>
2024-10-29 10:47:24 -0700

    Update example

--- javascript-sdk/examples/close-popup/close-popup.js
+++ javascript-sdk/examples/close-popup/close-popup.js
@@ -15,23 +15,25 @@ const CLOSE_POPUP_QUERY = `
 `;
 
 async function main() {
-  // Configure AgentQL with API key
-  configure({ apiKey: process.env.AGENTQL_API_KEY });
+    // Configure AgentQL with API key
+    configure({ apiKey: process.env.AGENTQL_API_KEY });
 
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
 
-  // Wrap the page to get access to the AgentQL's querying API
-  const page = wrap(await context.newPage());
+    // Wrap the page to get access to the AgentQL's querying API
+    const page = wrap(await context.newPage());
 
-  await page.goto(URL);
+    await page.goto(URL);
 
-  // Extract data using AgentQL API's queryElements() method
-  const response = await page.queryElements(CLOSE_POPUP_QUERY);
-  await response.popup_form.close_btn.click();
+    // Extract data using AgentQL API's queryElements() method
+    const response = await page.queryElements(CLOSE_POPUP_QUERY);
+    await response.popup_form.close_btn.click();
 
-  // Wait for 10 seconds to see the browser in action
-  await page.waitForTimeout(10000);
+    // Wait for 10 seconds to see the browser in action
+    await page.waitForTimeout(10000);
+
+    await browser.close();
 }
 
 main();
