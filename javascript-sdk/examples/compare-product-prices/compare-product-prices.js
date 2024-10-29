59f4cf717f9b9c39a5b2ecbd7d685aa819ece47c
jinyangTF <jinyang@tinyfish.io>
2024-10-29 10:44:03 -0700

    Update

--- javascript-sdk/examples/compare-product-prices/compare-product-prices.js
+++ javascript-sdk/examples/compare-product-prices/compare-product-prices.js
@@ -24,42 +24,42 @@ const PRODUCT_INFO_QUERY = `
  * Open the given URL in a new tab and fetch the price of the product.
  */
 async function fetchPrice(context, sessionUrl) {
-    // Create a page in a new tab in the broswer context and wrap it to get access to the AgentQL's querying API
-    const page = wrap(await context.newPage());
-    await page.goto(sessionUrl);
+  // Create a page in a new tab in the broswer context and wrap it to get access to the AgentQL's querying API
+  const page = wrap(await context.newPage());
+  await page.goto(sessionUrl);
 
-    // Search for the product
-    await page.waitForLoadState('networkidle');
-    const homeResponse = await page.queryElements(HOME_PAGE_QUERY);
-    await homeResponse.search_input.fill('Nintendo Switch - OLED Model White');
-    await homeResponse.search_button.click();
+  // Search for the product
+  await page.waitForLoadState('networkidle');
+  const homeResponse = await page.queryElements(HOME_PAGE_QUERY);
+  await homeResponse.search_input.fill('Nintendo Switch - OLED Model White');
+  await homeResponse.search_button.click();
 
-    // Fetch the price data from the page
-    const data = await page.queryData(PRODUCT_INFO_QUERY);
-    return data.product_price;
+  // Fetch the price data from the page
+  const data = await page.queryData(PRODUCT_INFO_QUERY);
+  return data.product_price;
 }
 
 /**
  * Fetch prices concurrently in the same browser session from multiple websites.
  */
 async function getPriceAcrossWebsites() {
-    const browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
 
-    // Open multiple tabs in the same browser context to fetch prices concurrently
-    const [bestbuyPrice, ebayPrice, telquestPrice] = await Promise.all([
-        fetchPrice(context, BESTBUY_URL),
-        fetchPrice(context, EBAY_URL),
-        fetchPrice(context, TELQUEST_URL)
-    ]);
+  // Open multiple tabs in the same browser context to fetch prices concurrently
+  const [bestbuyPrice, ebayPrice, telquestPrice] = await Promise.all([
+    fetchPrice(context, BESTBUY_URL),
+    fetchPrice(context, EBAY_URL),
+    fetchPrice(context, TELQUEST_URL),
+  ]);
 
-    console.log(`
+  console.log(`
     Price at BestBuy: ${bestbuyPrice}
     Price at eBay: ${ebayPrice}
     Price at Telquest: ${telquestPrice}
     `);
 
-    await browser.close();
+  await browser.close();
 }
 
 // Set the AgentQL API key via the `configure` method.
