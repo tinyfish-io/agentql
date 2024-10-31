ebc150ecf1dd6de7e0cbac7d78dd89c5891cdbe5
Zach Lyon <zachary@lyonfamily.org>
2024-10-30 21:26:45 -0400

    revert

--- javascript-sdk/examples/run-script-in-headless-browser/run-script-in-headless-browser.js
+++ javascript-sdk/examples/run-script-in-headless-browser/run-script-in-headless-browser.js
@@ -21,22 +21,20 @@ const STOCK_NUMBER_QUERY = `
 
 (async () => {
   // Launch a headless browser using Playwright.
-  const browser = await chromium.launch({ headless: true });
-  const normalPage = await browser.newPage();
-  await normalPage.goto(URL);
-
-  // Wrap a new page instance to use with AgentQL API
-  const wrappedPage = wrap(normalPage);
+  const browser = await chromium.launch({ headless: false });
+  // Create a new page in the browser and wrap it to get access to the AgentQL's querying API
+  const page = await wrap(await browser.newPage());
+  await page.goto(URL);
 
   // Use queryElements() method to locate the search box from the page.
-  const searchResponse = await wrappedPage.queryElements(SEARCH_QUERY);
+  const searchResponse = await page.queryElements(SEARCH_QUERY);
 
   // Use Playwright's API to fill the search box and press Enter.
   await searchResponse.search_products_box.type('Charmander');
   await normalPage.keyboard.press('Enter');
 
   // Use queryData() method to fetch the stock number from the page.
-  const stockResponse = await wrappedPage.queryData(STOCK_NUMBER_QUERY);
+  const stockResponse = await page.queryData(STOCK_NUMBER_QUERY);
   console.log(stockResponse);
 
   await browser.close();
