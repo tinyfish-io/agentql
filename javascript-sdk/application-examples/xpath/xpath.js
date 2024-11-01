311c22254a18e6cf16e2d5870c9211334872c1c8
Zach Lyon <zachary@lyonfamily.org>
2024-11-01 15:21:25 -0400

    linting

--- javascript-sdk/application-examples/xpath/xpath.js
+++ javascript-sdk/application-examples/xpath/xpath.js
@@ -14,24 +14,24 @@ const QUERY = `
 {
     search_products_box
 }
-`
+`;
 
 async function main() {
-	// Launch a headless browser using Playwright.
-	const browser = await chromium.launch({ headless: false });
+  // Launch a headless browser using Playwright.
+  const browser = await chromium.launch({ headless: false });
 
-	// Create a new page in the browser and wrap it to get access to the AgentQL's querying API
-	const page = await wrap(await browser.newPage());
-	await page.goto(URL);
+  // Create a new page in the browser and wrap it to get access to the AgentQL's querying API
+  const page = await wrap(await browser.newPage());
+  await page.goto(URL);
 
-	// Use queryElements() method to locate the search box from the page.
-	const response = await page.queryElements(QUERY);
+  // Use queryElements() method to locate the search box from the page.
+  const response = await page.queryElements(QUERY);
 
-	// Get the XPath of the search box element.
-	console.log('XPath:', await xPath(response.search_products_box));
+  // Get the XPath of the search box element.
+  console.log('XPath:', await xPath(response.search_products_box));
 
-	// Close the browser.
-	await browser.close();
+  // Close the browser.
+  await browser.close();
 }
 
 main();
