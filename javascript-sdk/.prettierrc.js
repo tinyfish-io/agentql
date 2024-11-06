30907731fbb8a38c962a119948da8f76062bbccc
Tianling Feng <frank@tinyfish.io>
2024-11-05 19:46:48 -0600

    Refactor agentql repo

--- javascript-sdk/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  trailingComma: 'all',
-  singleQuote: true,
-  importOrder: ['<THIRD_PARTY_MODULES>', '^@/types/(.*)$', '^@/lib/(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  importOrderCaseInsensitive: false,
-};
