8f05ede112f4357c5bd8cdf8f072cc3e8e9362fd
jayfish0 <jason@tinyfish.io>
2025-01-09 12:50:00 -0800

    change example

--- examples/python/collect_paginated_ecommerce_listing_data/main.py
+++ examples/python/collect_paginated_ecommerce_listing_data/main.py
@@ -11,26 +11,38 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
     with sync_playwright() as playwright, playwright.chromium.launch(headless=False) as browser:
         page = agentql.wrap(browser.new_page())
-        page.goto("https://wristler.eu/sellers?page=1")
+        page.goto("https://books.toscrape.com/")
 
+        # define the query to extract product names, prices, and ratings
         QUERY = """
         {
-            sellers[] {
+            books[] {
                 name
+                price
+                rating
             }
         }
         """
 
-        for i in range(3):
-            response = page.query_data(QUERY)
-            with open(f"./sellers_{i}.json", "w") as f:
-                json.dump(response, f, indent=4)
+        books = []
 
-            log.debug("Navigating to next page...")
+        # Aggregate the first 50 book names, prices and ratings
+        while len(books) < 50:
+            # collect data from the current page
+            response = page.query_data(QUERY)
+
+            # limit the total number of books to 50
+            if len(response["books"]) + len(books) > 50:
+                books.extend(response["books"][:50 - len(books)])
+            else:
+                books.extend(response["books"])
+
+            # get the pagination info from the current page
             pagination_info = page.get_pagination_info()
 
             # attempt to navigate to next page
             if pagination_info.has_next_page:
                 pagination_info.navigate_to_next_page()
 
-        page.wait_for_timeout(1000)
+        with open(f"./books.json", "w") as f:
+            json.dump(books, f, indent=4)
