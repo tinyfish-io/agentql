0a975e66c478460562e55ac3d5aa2560996a4f96
Pasha Dudka <dudka.pavel@gmail.com>
2025-06-24 15:00:13 -0700

    Fix example

--- examples/python/use_external_browser/main.py
+++ examples/python/use_external_browser/main.py
@@ -1,10 +1,10 @@
 """This example demonstrates how to interact with an external browser with AgentQL."""
 
-import os
+import asyncio
 
 import agentql
-import httpx
-from playwright.sync_api import sync_playwright
+from agentql.tools.async_api import BrowserSession, UserAgentPreset, create_browser_session
+from playwright.async_api import async_playwright
 
 URL = "https://scrapeme.live/shop"
 
@@ -20,48 +20,42 @@ STOCK_QUERY = """
 }
 """
 
-AGENTQL_API_KEY = os.getenv("AGENTQL_API_KEY")
+
+async def get_remote_browser_url() -> str:
+    """This function creates a new browser session."""
+    # Create a browser session with Windows user agent preset
+    session: BrowserSession = await create_browser_session(ua_preset=UserAgentPreset.WINDOWS)
+    cdp_url = session.cdp_url
+    print(f"Remote browser URL: {cdp_url}")
+    print(f"Page streaming URL: {session.get_page_streaming_url(0)}")
+    return cdp_url
 
 
-def get_remote_browser_url():
-    """This function allocates a new browser instance."""
-    with httpx.Client() as client:
-        response = client.post(
-            "https://api.agentql.com/tetra/sessions",
-            headers={"X-API-Key": AGENTQL_API_KEY},
-        )
-        response.raise_for_status()
-        session = response.json()
-        cdp_url = session["cdp_url"]
-        print(f"Remote browser URL: {cdp_url}")
-        return cdp_url
-
-
-def run_in_external_browser():
+async def run_in_external_browser():
     """This function demonstrates how to open and interact with a new page using remote browser."""
-    browser_url = get_remote_browser_url()
-    with sync_playwright() as p:
+    browser_url = await get_remote_browser_url()
+    async with async_playwright() as p:
         # Connect to the browser via Chrome DevTools Protocol
-        browser = p.chromium.connect_over_cdp(browser_url)
+        browser = await p.chromium.connect_over_cdp(browser_url)
 
         # Create a new tab in the browser window and wrap it to get access to the AgentQL's querying API
-        page = browser.new_page()
-        page = agentql.wrap(page)
+        page = await browser.new_page()
+        page = await agentql.wrap_async(page)
 
-        page.goto(URL)
+        await page.goto(URL)
 
         # Use query_elements() method to locate the search product box from the page
-        response = page.query_elements(SEARCH_QUERY)
+        response = await page.query_elements(SEARCH_QUERY)
 
         # Use Playwright's API to fill the search box and press Enter
-        response.search_products_box.type("Charmander")
-        page.keyboard.press("Enter")
+        await response.search_products_box.type("Charmander")
+        await page.keyboard.press("Enter")
 
         # Use query_data() method to fetch the stock number from the page
-        response = page.query_data(STOCK_QUERY)
+        response = await page.query_data(STOCK_QUERY)
 
         print(response)
 
 
 if __name__ == "__main__":
-    run_in_external_browser()
+    asyncio.run(run_in_external_browser())
